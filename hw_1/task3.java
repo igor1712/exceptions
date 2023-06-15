e4f1d357b1cddde561a6e5ad870b9ff908d4bab9
igor <kishm2013@mail.ru>
2023-06-15 19:32:31 +0300

    домашнее задание 1

---
+++ hw_1/task3.java
@@ -0,0 +1,43 @@
+package hw_1;
+
+import java.util.Random;
+
+public class task3 {
+    public static void main(String[] args) {
+        int[] firstArray = getFilledArray(10);
+        int[] secondArray = getFilledArray(10);
+
+        int[] divArray = divArrays(firstArray, secondArray);
+        printArray(divArray);
+
+        int[] badArray = divArrays(null, secondArray);
+    }
+
+    public static int[] divArrays(int[] firstArray, int[] secondArray) {
+        if (firstArray == null || secondArray == null) throw new RuntimeException("Нет массивов!!!");
+        if(firstArray.length != secondArray.length) throw new RuntimeException("Длины массивов не равны!!!");
+
+
+        int[] resultArr = new int[firstArray.length];
+        for (int i = 0; i < resultArr.length; i++) {
+            resultArr[i] = firstArray[i]/secondArray[i];
+        }
+        return resultArr;
+    }
+
+    public static int[] getFilledArray(int length){
+        int[] array = new int[length];
+        Random rnd = new Random();
+        for (int i = 0; i < length; i++) {
+            array[i] = rnd.nextInt(1,20);
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array){
+        for (int number: array){
+            System.out.println(number);
+        }
+    }
+    
+}
